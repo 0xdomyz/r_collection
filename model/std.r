6b0912ccd013eb3b68448a2766793728b4aaa4de
0xdomyz <yzdominic@outlook.com>
2022-03-28 21:06:22 +1100

    reorganize lm into std and weights

---
+++ model/std.r
@@ -0,0 +1,10 @@
+std <- function(v) (v-mean(v, na.rm = TRUE))/sqrt(var(v, na.rm = TRUE))
+
+
+if (identical(environment(), globalenv())){
+    x <- c(1,2,3,4,5,NA,7)
+    mean(x, na.rm = TRUE)
+    var(x, na.rm = TRUE)
+    std(x)
+    # [1] -1.2344268 -0.7715167 -0.3086067  0.1543033  0.6172134         NA  1.5430335
+}
--- model/weights.r
+++ model/weights.r
@@ -1,10 +1,3 @@
-# Examples
-# -----------
-# > x <- c(1,2,3,4,5,NA,7)
-# > mean(x, na.rm = TRUE)
-# > var(x, na.rm = TRUE)
-# > std(x)
-# [1] -1.2344268 -0.7715167 -0.3086067  0.1543033  0.6172134         NA  1.5430335
 std <- function(v){
     (v-mean(v, na.rm = TRUE))/sqrt(var(v, na.rm = TRUE))
 }
