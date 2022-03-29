f8a53099794f65c59019059cbf2228cad9b5e570
0xdomyz <yzdominic@outlook.com>
2022-03-29 17:24:04 +1100

    improve std doc

--- model/std.r
+++ model/std.r
@@ -1,3 +1,8 @@
+# Examples
+# ----------
+# > x <- c(1,2,3,4,5,NA,7)
+# > std(x)
+# [1] -1.2344268 -0.7715167 -0.3086067  0.1543033  0.6172134         NA  1.5430335
 std <- function(v) (v-mean(v, na.rm = TRUE))/sqrt(var(v, na.rm = TRUE))
 
 
@@ -6,5 +11,4 @@ if (identical(environment(), globalenv())){
     mean(x, na.rm = TRUE)
     var(x, na.rm = TRUE)
     std(x)
-    # [1] -1.2344268 -0.7715167 -0.3086067  0.1543033  0.6172134         NA  1.5430335
 }
