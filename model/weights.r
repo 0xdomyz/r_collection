f31367d4da157206101506fd7bfd2d634f661488
unknown <yzdominic@outlook.com>
2022-04-05 16:47:33 +1000

    add formula func and example app to weights

--- model/weights.r
+++ model/weights.r
@@ -1,3 +1,9 @@
+import::here(here, here)
+import::here('rsq.r', make_adjr2, .directory = here('model'))
+import::here(stringr, str_c)
+
+std <- function(v) (v-mean(v, na.rm = TRUE))/sqrt(var(v, na.rm = TRUE))
+
 # Examples
 # -----------
 # > std <- function(v) (v-mean(v, na.rm = TRUE))/sqrt(var(v, na.rm = TRUE))
@@ -32,8 +38,33 @@ parameters_to_weights <- function(parameters){
     )
 }
 
-weights_to_parameters <- function(weights, signs, parameters){
-    weights*signs*sum(abs(parameters))
+#does not give intercept back
+weights_to_parameters <- function(weights, signs, calibration_a){
+    weights*signs*sum(abs(calibration_a))
+}
+
+# Examples
+# -----------
+# > fit = lm(mpg ~ cyl+disp, mtcars)
+# > ce = fit$coefficients; ce
+# (Intercept)         cyl        disp
+# 34.66099474 -1.58727681 -0.02058363
+# > fml = parameters_to_formula(ce, 'mpg'); fml
+# mpg ~ 34.6609947413328 + -1.58727680900718 * cyl + -0.0205836333707016 *
+#     disp
+# <environment: 0x000000000b9ac710>
+# > pred_from_fml = eval(parse(text = fml), mtcars)
+# > pred_from_fit = predict(fit, mtcars)
+# > names(pred_from_fit) = NULL
+# > sum(abs(pred_from_fml - pred_from_fit) > 1e10)
+# [1] 0
+parameters_to_formula = function(parameters, target){
+    inter = parameters[1]
+    non_inter = parameters[2:length(parameters)]
+    fml_no_inter = str_c(non_inter, names(non_inter), sep = ' * ', collapse = ' + ')
+    fml = str_c(inter, fml_no_inter, sep=' + ')
+    fml_target = str_c(target, fml, sep=' ~ ')
+    as.formula(fml_target)
 }
 
 
@@ -67,5 +98,30 @@ parameters_to_norm_parameters <- function(intercept, parameters, means, stds) {
 }
 
 if (identical(environment(), globalenv())){
-    1
+    data = purrr::map(mtcars, std) |> dplyr::bind_cols()
+    fit = lm(mpg ~ cyl + disp + hp + drat, data)
+    ce = fit$coefficients
+    inter = ce[1]
+    w = parameters_to_weights(ce); w$weights
+    make_adjr2(predict(fit, data), data$mpg, 4)
+
+    man_wts = c(0.25, 0.3, 0.25, 0.2); names(man_wts) = names(w$weights); man_wts
+    man_para = c(inter, weights_to_parameters(man_wts, w$signs, w$calibration[1]))
+    fml = parameters_to_formula(man_para, 'mpg')
+    make_adjr2(eval(parse(text = fml), data), data$mpg, 4)
+
+    man_wts = c(0.25, 0.25, 0.25, 0.25); names(man_wts) = names(w$weights); man_wts
+    man_para = c(inter, weights_to_parameters(man_wts, w$signs, w$calibration[1]))
+    fml = parameters_to_formula(man_para, 'mpg')
+    make_adjr2(eval(parse(text = fml), data), data$mpg, 4)
+
+    man_wts = c(0.3, 0.3, 0.3, 0.1); names(man_wts) = names(w$weights); man_wts
+    man_para = c(inter, weights_to_parameters(man_wts, w$signs, w$calibration[1]))
+    fml = parameters_to_formula(man_para, 'mpg')
+    make_adjr2(eval(parse(text = fml), data), data$mpg, 4)
+
+    man_wts = c(0.33, 0.33, 0.34, 0); names(man_wts) = names(w$weights); man_wts
+    man_para = c(inter, weights_to_parameters(man_wts, w$signs, w$calibration[1]))
+    fml = parameters_to_formula(man_para, 'mpg')
+    make_adjr2(eval(parse(text = fml), data), data$mpg, 3)
 }
