0d4c3fb64e02ad98dcdd885e044f54a1ac7c81d8
unknown <yzdominic@outlook.com>
2022-05-28 20:03:10 +1000

    update std r

--- model/std.r
+++ model/std.r
@@ -6,6 +6,25 @@
 std <- function(v) (v-mean(v, na.rm = TRUE))/sqrt(var(v, na.rm = TRUE))
 
 
+if (FALSE) {
+  #apply std on data
+  std <- function(v) (v-mean(v, na.rm = TRUE))/sqrt(var(v, na.rm = TRUE))
+  data = purrr::map(mtcars, std) |> dplyr::bind_cols()
+  data
+  
+  #calc mean/std
+  means = purrr::map_df(mtcars, function(v) mean(v, na.rm = TRUE))
+  as.numeric(means)
+  
+  stds = purrr::map_df(mtcars, function(v) sqrt(var(v, na.rm = TRUE)))
+  as.numeric(stds)
+  
+  #apply mean/std on diff population
+  purrr::pmap(list(mtcars*2-1, as.numeric(means), as.numeric(stds)),
+    function(v, m, std) (v-m)/std) |> dplyr::bind_cols()
+}
+
+
 if (identical(environment(), globalenv())){
     x <- c(1,2,3,4,5,NA,7)
     mean(x, na.rm = TRUE)
