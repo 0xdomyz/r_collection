d952f046ae69a4d3b56a6b9646442f29c2191c1a
unknown <yzdominic@outlook.com>
2022-08-08 19:21:26 +1000

    udpate import here examples

--- layout/here_tute/inner/code.r
+++ layout/here_tute/inner/code.r
@@ -1,3 +1,17 @@
-file = here::here("inner", "inner2", "inner2.txt")
+dir = here::here("inner", "inner2")
+import::here("inner2_code.r", a_func, .directory = dir)
+print_dir = function() {print(dir)}
+print(a_func(1))
 
+file = here::here("inner", "inner2", "inner2.txt")
 print(readLines(file))
+print_file = function() {print(file)}
+
+print("inner wd:")
+print(getwd())
+
+#at project outer root
+#r
+#import::here("code.r", print_file, .directory = "layout/here_tute/inner")
+#print_file
+
---
+++ layout/here_tute/inner/inner2/inner2_code.r
@@ -0,0 +1,6 @@
+a_func = function(x) {
+    x+2
+}
+
+print("inner2 wd:")
+print(getwd())
