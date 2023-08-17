a0538b5c395f3a3e48350902ba0f215854728975
0xdomyz <yzdominic@outlook.com>
2023-08-17 20:22:02 +1000

    add svae text

---
+++ data/text/text.r
@@ -0,0 +1,27 @@
+# make a formula
+fml <- "z~ 1+ 3.22*x^2 + 1.89*y^2 * z"
+fml <- as.formula(fml)
+fml
+
+# make into a char
+fml_char <- as.character(fml)
+fml_char
+
+# save fml into a plain txt file
+writeLines(fml_char, "data/text/text.txt")
+
+
+# save output of lm into a plain txt file
+fit <- lm(
+    "mpg ~ cyl + disp + hp + drat + wt + qsec + vs + am + gear + carb",
+    mtcars
+)
+
+fit
+
+# save output of lm into a plain txt file
+sink("data/text/output.txt")
+fit
+sink()
+
+fit
